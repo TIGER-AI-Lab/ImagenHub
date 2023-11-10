951898a54fa1b4778faab055ea1ee187217995cb
Max Ku <m3ku@uwaterloo.ca>
2023-11-10 17:15:44 -0500

    add script

---
+++ download_Subject-Driven_IG_weights.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the directory of the current script.
+SCRIPT_DIR=$(dirname "$0")
+
+# Change to the directory where the script is located.
+cd "$SCRIPT_DIR"
+
+# Check if 'checkpoints' directory exists in that directory. If not, create it.
+if [ ! -d "checkpoints" ]; then
+    mkdir checkpoints
+fi
+
+# Change directory to 'checkpoints'.
+cd checkpoints
+
+# Install git lfs.
+git lfs install
+
+# Clone the specified repository.
+git clone https://huggingface.co/ImagenHub/ImagenHub_Subject-Driven_IG
+
+echo "Downloaded Subject-Driven_IG to checkpoints."
+
