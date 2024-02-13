13b3c27ec868c7e376ebff4dcf75a843992320ce
SunShizhuo <sunsz007@163.com>
2024-02-12 22:12:00 -0500

    Modify Wuerstchen by pipeline in diffusers

--- src/imagen_hub/infermodels/wuerstchen.py
+++ src/imagen_hub/infermodels/wuerstchen.py
@@ -1,10 +1,8 @@
 import torch
-from torchvision import transforms
-from imagen_hub.pipelines.wuerstchen import WuerstchenPipeline
-
-
-def tensor2PIL(image):
-    return transforms.ToPILImage()(image).convert('RGB')
+from diffusers import WuerstchenPriorPipeline, WuerstchenDecoderPipeline
+from diffusers.pipelines.wuerstchen import DEFAULT_STAGE_C_TIMESTEPS
+import accelerate
+import peft
 
 
 class WuerstchenModel():
@@ -15,14 +13,20 @@ class WuerstchenModel():
         https://huggingface.co/docs/diffusers/v0.23.0/en/api/pipelines/wuerstchen#w%C3%BCrstchen
     """
 
-    def __init__(self, device="cuda"):
+    def __init__(self, device="cuda", dtype=torch.float16):
         """
         Initializes the Wuerstchen Model
+        Using the Pipeline provided in diffusers
 
         Args:
             device (str): The device for running the pipeline ("cuda" or "cpu"). Default is "cuda".
         """
-        self.pipe = WuerstchenPipeline(device=device)
+        self.prior_pipeline = WuerstchenPriorPipeline.from_pretrained("warp-ai/wuerstchen-prior",
+                                                                      torch_dtype=dtype,
+                                                                      low_cpu_mem_usage=True).to(device)
+        self.decoder_pipelne = WuerstchenDecoderPipeline.from_pretrained("warp-ai/wuerstchen",
+                                                                         torch_dtype=dtype,
+                                                                         low_cpu_mem_usage=True).to(device)
 
     def infer_one_image(self, caption, negative_caption, seed=42, height=1024, width=1024):
         """
@@ -40,45 +44,16 @@ class WuerstchenModel():
 
         """
         torch.manual_seed(seed)
-        batch_size = 1
-        latent_height = 128 * (height // 128) // (1024 // 24)
-        latent_width = 128 * (width // 128) // (1024 // 24)
-        prior_features_shape = (batch_size, 16, latent_height, latent_width)
-        prior_inference_steps = {2 / 3: 20, 0.0: 10}
-        effnet_features_shape = (batch_size, 16, 12, 12)
-        prior_cfg = 6
-        prior_sampler = "ddpm"
-        generator_steps = 12
-        generator_cfg = None
-        generator_sampler = "ddpm"
-
-        clip_text_embeddings, clip_text_embeddings_uncond = self.pipe.embed_clip(caption, negative_caption)
-        effnet_embeddings_uncond = torch.zeros(effnet_features_shape).to(self.pipe.device)
-        generator_latent_shape = (batch_size, 4, int(latent_height * (256 / 24)), int(latent_width * (256 / 24)))
-        with torch.cuda.amp.autocast(dtype=torch.bfloat16), torch.no_grad():
-            sampled = None
-            for t_end, steps in prior_inference_steps.items():
-                sampled = self.pipe.diffuzz.sample(self.pipe.model, {'c': clip_text_embeddings}, x_init=sampled,
-                                                   unconditional_inputs={"c": clip_text_embeddings_uncond},
-                                                   shape=prior_features_shape,
-                                                   timesteps=steps, cfg=prior_cfg, sampler=prior_sampler,
-                                                   t_start=t_start, t_end=t_end)[-1]
-                t_start = t_end
-            sampled = sampled.mul(42).sub(1)
-
-            clip_text_embeddings, clip_text_embeddings_uncond = self.pipe.embed_clip(caption, negative_caption,
-                                                                                     batch_size)
-            sampled_images_original = \
-                self.pipe.diffuzz.sample(self.pipe.generator, {'effnet': sampled, 'clip': clip_text_embeddings},
-                                         generator_latent_shape, t_start=1.0, t_end=0.00,
-                                         timesteps=generator_steps, cfg=generator_cfg,
-                                         sampler=generator_sampler,
-                                         unconditional_inputs={
-                                             'effnet': effnet_embeddings_uncond,
-                                             'clip': clip_text_embeddings_uncond,
-                                         })[-1]
-
-        image = self.pipe.decode(sampled_images_original)
-        image = image.reshape(3, height, width)
-        img = tensor2PIL(image)
-        return img
+        prior_output = self.prior_pipeline(prompt=caption,
+                                           height=height,
+                                           width=width,
+                                           timesteps=DEFAULT_STAGE_C_TIMESTEPS,
+                                           negative_caption=negative_caption,
+                                           guidance_scale=4.0,
+                                           num_images_per_prompt=1)
+        decoder_output = self.decoder_pipelne(image_embeddings=prior_output.image_embeddings,
+                                              prompt=caption,
+                                              negative_caption=negative_caption,
+                                              guidance_scale=0.0,
+                                              output_type='pil')
+        return decoder_output.images[0]
