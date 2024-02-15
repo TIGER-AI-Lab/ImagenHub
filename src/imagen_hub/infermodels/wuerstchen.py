2ba8c66148397cedbaa6cfe5709546c8858175fa
SunShizhuo <sunsz007@163.com>
2024-02-15 14:06:13 -0500

    Modify Wuerstchen by agreeing its argument in infer_one_image function.

--- src/imagen_hub/infermodels/wuerstchen.py
+++ src/imagen_hub/infermodels/wuerstchen.py
@@ -29,31 +29,26 @@ class Wuerstchen():
                                                                          torch_dtype=dtype,
                                                                          low_cpu_mem_usage=True).to(device)
 
-    def infer_one_image(self, caption, negative_caption, seed=42, height=1024, width=1024):
+    def infer_one_image(self, prompt: str = None, seed: int = 42):
         """
         Generate a image using Wuerstchen Model
 
         Args:
-            caption: The text provided for the generation
-            negative_caption: The negative text provided for the generation
+            prompt: The text provided for the generation.
             seed (int, optional): The seed for random generator. Default is 42.
-            height: height of the image
-            width: width of the image
-
         Returns:
             PIL.Image.Image: The generated image.
 
         """
+        negative_caption = None
         torch.manual_seed(seed)
-        prior_output = self.prior_pipeline(prompt=caption,
-                                           height=height,
-                                           width=width,
+        prior_output = self.prior_pipeline(prompt=prompt,
                                            timesteps=DEFAULT_STAGE_C_TIMESTEPS,
                                            negative_caption=negative_caption,
                                            guidance_scale=4.0,
                                            num_images_per_prompt=1)
         decoder_output = self.decoder_pipelne(image_embeddings=prior_output.image_embeddings,
-                                              prompt=caption,
+                                              prompt=prompt,
                                               negative_caption=negative_caption,
                                               guidance_scale=0.0,
                                               output_type='pil')
