30ec593fdd980ee595f19517c70d76362cd82dc6
Max Ku <m3ku@uwaterloo.ca>
2024-02-25 17:04:51 -0500

    update weight

--- src/imagen_hub/infermodels/sdxl.py
+++ src/imagen_hub/infermodels/sdxl.py
@@ -63,7 +63,7 @@ class SDXLLightning():
         from huggingface_hub import hf_hub_download
         from safetensors.torch import load_file
         base = "stabilityai/stable-diffusion-xl-base-1.0"
-        ckpt = "sdxl_lightning_4step_unet.pth"
+        ckpt = "sdxl_lightning_4step_unet.safetensors"
         unet = UNet2DConditionModel.from_config(base, subfolder="unet").to("cuda", torch.float16)
         unet.load_state_dict(load_file(hf_hub_download(weight, ckpt), device="cuda"))
         self.pipe = StableDiffusionXLPipeline.from_pretrained(
