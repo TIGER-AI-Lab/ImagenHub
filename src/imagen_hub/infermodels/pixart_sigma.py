273249663ab5ef1480c101fb0a86e5b471986c42
Max Ku <m3ku@uwaterloo.ca>
2024-04-29 20:10:03 -0400

    fix

--- src/imagen_hub/infermodels/pixart_sigma.py
+++ src/imagen_hub/infermodels/pixart_sigma.py
@@ -1,28 +1,35 @@
 import torch
 
-
 class PixArtSigma:
     """
     PixArtSigma for T2I tasks
     Reference: https://huggingface.co/spaces/PixArt-alpha/PixArt-Sigma/blob/main/app.py
     """
 
-    def __init__(
-        self, device="cuda", weight="PixArt-alpha/pixart_sigma_sdxlvae_T5_diffusers"
-    ):
+    def __init__(self, device="cuda"):
         """
         Attributes:
             pipe (AutoPipelineForText2Image): The underlying image generation pipeline object.
 
         Args:
             device (str, optional): The device on which the pipeline should run. Default is "cuda".
-            weight (str, optional): The pretrained model weights for image generation. Default is "PixArt-alpha/pixart_sigma_sdxlvae_T5_diffusers".
         """
-        from imagen_hub.pipelines.pixart.sigma.diffusers_patches import PixArtSigmaPipeline
+        from imagen_hub.pipelines.pixart.sigma.diffusers_patches import PixArtSigmaPipeline, pixart_sigma_init_patched_inputs
+        from diffusers import Transformer2DModel
 
-        self.pipe = PixArtSigmaPipeline.from_pretrained(
-            weight,
+        print("Changing _init_patched_inputs method of diffusers.models.Transformer2DModel using diffusers_patches.pixart_sigma_init_patched_inputs")
+        setattr(Transformer2DModel, '_init_patched_inputs', pixart_sigma_init_patched_inputs)
+
+        transformer = Transformer2DModel.from_pretrained(
+            "PixArt-alpha/PixArt-Sigma-XL-2-512-MS", # No idea why PixArt-alpha/PixArt-Sigma-XL-2-1024-MS causes missing keys
+            subfolder='transformer',
             torch_dtype=torch.float16,
+        )
+        self.pipe = PixArtSigmaPipeline.from_pretrained(
+            "PixArt-alpha/pixart_sigma_sdxlvae_T5_diffusers",
+            transformer=transformer,
+            torch_dtype=torch.float16,
+            use_safetensors=True,
         ).to(device)
 
     def infer_one_image(self, prompt: str = None, seed: int = 42):
