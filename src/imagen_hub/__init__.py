cb154d7ebcbe1dfab770acfe822ef66673bfe010
Max Ku <m3ku@uwaterloo.ca>
2024-07-12 02:06:18 -0400

    Adding Kolor

--- src/imagen_hub/__init__.py
+++ src/imagen_hub/__init__.py
@@ -1,2 +1,8 @@
+import os
 from ._version import __version__
+
+MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "checkpoints"))
+if os.environ.get("IMAGE_MODEL_PATH"):
+    MODEL_PATH = os.environ.get("IMAGE_MODEL_PATH")
+
 from .infermodels import load, get_model, load_model
