c067bb792158fbb08a5495c78754e8416a82ce4f
Max Ku <m3ku@uwaterloo.ca>
2024-08-28 16:07:42 -0400

    Updating PixArtSigma

--- src/imagen_hub/infermodels/pixart_sigma.py
+++ src/imagen_hub/infermodels/pixart_sigma.py
@@ -3,7 +3,8 @@ import torch
 class PixArtSigma:
     """
     PixArtSigma for T2I tasks
-    Reference: https://huggingface.co/spaces/PixArt-alpha/PixArt-Sigma/blob/main/app.py
+    Reference: https://huggingface.co/spaces/PixArt-alpha/PixArt-Sigma/blob/main/app.py <- Obsolete
+    New Reference: https://huggingface.co/docs/diffusers/api/pipelines/pixart_sigma
     """
 
     def __init__(self, device="cuda"):
@@ -14,22 +15,20 @@ class PixArtSigma:
         Args:
             device (str, optional): The device on which the pipeline should run. Default is "cuda".
         """
-        from imagen_hub.pipelines.pixart.sigma.diffusers_patches import PixArtSigmaPipeline, pixart_sigma_init_patched_inputs
-        from diffusers import Transformer2DModel
+        from transformers import T5EncoderModel
+        from diffusers import PixArtSigmaPipeline
 
-        print("Changing _init_patched_inputs method of diffusers.models.Transformer2DModel using diffusers_patches.pixart_sigma_init_patched_inputs")
-        setattr(Transformer2DModel, '_init_patched_inputs', pixart_sigma_init_patched_inputs)
-
-        transformer = Transformer2DModel.from_pretrained(
-            "PixArt-alpha/PixArt-Sigma-XL-2-512-MS", # No idea why PixArt-alpha/PixArt-Sigma-XL-2-1024-MS causes missing keys
-            subfolder='transformer',
-            torch_dtype=torch.float16,
+        text_encoder = T5EncoderModel.from_pretrained(
+            "PixArt-alpha/PixArt-Sigma-XL-2-1024-MS",
+            subfolder="text_encoder",
+            load_in_8bit=True,
+            device_map="auto",
         )
         self.pipe = PixArtSigmaPipeline.from_pretrained(
-            "PixArt-alpha/pixart_sigma_sdxlvae_T5_diffusers",
-            transformer=transformer,
-            torch_dtype=torch.float16,
-            use_safetensors=True,
+            "PixArt-alpha/PixArt-Sigma-XL-2-1024-MS",
+            text_encoder=text_encoder,
+            transformer=None,
+            device_map="balanced"
         )
         self.device = device
 
